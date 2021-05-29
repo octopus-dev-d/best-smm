1c0c0ae7f3ea1b3721610bc123bdc6d1381d6222
Octopus <octopus.dev@yandex.kz>
2021-05-30 00:05:14 +0600

    добавлены слайдеры для блоков наши клиенты и отзывы, добавлены якоря, частично сделан слайдер для портфолио

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-    $('.swiper-slide, .projects-slide').slick({
+    $('.swiper-slide, .projects-slide, .logo-container').slick({
         arrows:true,
         autoplay:true,
-        autoplaySpeed:5000
+        adaptiveHeight: true,
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        speed: 1000,
+        easing: 'ease',
+        autoplaySpeed:3000,
+    });
+});
+
+$(document).ready(function() {
+    $('.rewiews-container').slick({
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        easing: 'ease',
+        autoplaySpeed:3000,
+    });
+});
+
+$(document).ready(function() {
+    $('.portfolio-cards').slick({
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        speed: 1000,
+        easing: 'ease',
+        autoplaySpeed:3000,
     });
 });
